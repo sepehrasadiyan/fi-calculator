a3c885da5ea77423eb69e364dee79a744de5cdf4
Sepehr <asadiyansepehr@gmail.com>
2025-08-17 23:02:44 +0330

    Init

---
+++ src/main/java/me/fi_calculator/fi_calculator/FiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package me.fi_calculator.fi_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/fi_calculator/fi_calculator/FiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.fi_calculator.fi_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
