b5f3f06019f967f94d70f4db712cd7f17cd9e2b2
Sepehr <asadiyansepehr@gmail.com>
2025-08-18 20:36:30 +0330

    add more skelton

--- src/main/java/me/fi_calculator/fi_calculator/repository/RoleRepository.java
+++ src/main/java/me/fi_calculator/fi_calculator/repository/RoleRepository.java
@@ -1,7 +1,6 @@
 package me.fi_calculator.fi_calculator.repository;
 
 import me.fi_calculator.fi_calculator.domain.RoleEntity;
-import org.hibernate.validator.constraints.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/main/java/me/fi_calculator/fi_calculator/services/UserService.java
@@ -0,0 +1,4 @@
+package me.fi_calculator.fi_calculator.services;
+
+public class UserService {
+}
