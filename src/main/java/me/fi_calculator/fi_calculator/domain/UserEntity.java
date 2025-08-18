5f2b41d0ed4b8c1a81823099183bac2dd9f8033b
Sepehr <asadiyansepehr@gmail.com>
2025-08-18 18:53:13 +0330

    Start security things

---
+++ src/main/java/me/fi_calculator/fi_calculator/domain/UserEntity.java
@@ -0,0 +1,8 @@
+package me.fi_calculator.fi_calculator.domain;
+
+import jakarta.persistence.Entity;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class UserEntity implements UserDetails {
+}
