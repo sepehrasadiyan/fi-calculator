09dfd2a8b5fe191c1defefe11ad0a952ab43bb16
Sepehr <asadiyansepehr@gmail.com>
2025-08-18 19:00:27 +0330

    create user skelton

--- src/main/java/me/fi_calculator/fi_calculator/FiCalculatorApplication.java
+++ src/main/java/me/fi_calculator/fi_calculator/FiCalculatorApplication.java
@@ -2,8 +2,10 @@ package me.fi_calculator.fi_calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FiCalculatorApplication {
 
     public static void main(String[] args) {
--- src/main/java/me/fi_calculator/fi_calculator/domain/UserEntity.java
+++ src/main/java/me/fi_calculator/fi_calculator/domain/UserEntity.java
@@ -1,8 +1,85 @@
 package me.fi_calculator.fi_calculator.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
 @Entity
-public class UserEntity implements UserDetails {
+@Setter
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class UserEntity implements UserDetails, Persistable<UUID> {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 190)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdAt == null;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
