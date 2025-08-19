a093099fa22bb077057c6b3537df391ff8333263
Sepehr <asadiyansepehr@gmail.com>
2025-08-19 18:33:44 +0330

    add JWT dependency

--- src/main/java/me/fi_calculator/fi_calculator/services/JwtService.java
+++ src/main/java/me/fi_calculator/fi_calculator/services/JwtService.java
@@ -3,11 +3,21 @@ package me.fi_calculator.fi_calculator.services;
 import me.fi_calculator.fi_calculator.config.app.AppSettings;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.UUID;
+
 @Service
 public class JwtService {
+
     private final AppSettings settings;
 
     public JwtService(AppSettings settings) {
         this.settings = settings;
     }
+
+    public String generateAccess(String subjectEmail, UUID userId, Set<String> roleCodes) {
+        return null;
+    }
+
+
 }
