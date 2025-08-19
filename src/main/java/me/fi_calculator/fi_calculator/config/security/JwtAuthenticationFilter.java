ee1d97f29422b1a732d9be0b98819d08c660332e
Sepehr <asadiyansepehr@gmail.com>
2025-08-19 19:07:21 +0330

    moving with OncePerRequest and chain

--- src/main/java/me/fi_calculator/fi_calculator/config/security/JwtAuthenticationFilter.java
+++ src/main/java/me/fi_calculator/fi_calculator/config/security/JwtAuthenticationFilter.java
@@ -1,5 +1,28 @@
 package me.fi_calculator.fi_calculator.config.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.fi_calculator.fi_calculator.services.JwtService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 //Todo
-public class JwtAuthenticationFilter {
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res,
+                                    FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, res);
+    }
 }
--- src/main/java/me/fi_calculator/fi_calculator/config/security/SecurityConfig.java
+++ src/main/java/me/fi_calculator/fi_calculator/config/security/SecurityConfig.java
@@ -2,11 +2,19 @@ package me.fi_calculator.fi_calculator.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
