8243495c82efc33ad14d9b88ace3557f42c32016
Sepehr <asadiyansepehr@gmail.com>
2025-08-20 20:28:33 +0330

    inject Orchestrator as need

--- src/main/java/me/fi_calculator/fi_calculator/controller/api/CalculatorController.java
+++ src/main/java/me/fi_calculator/fi_calculator/controller/api/CalculatorController.java
@@ -5,6 +5,7 @@ import me.fi_calculator.fi_calculator.config.app.ShareContext;
 import me.fi_calculator.fi_calculator.domain.dtos.FiRequest;
 import me.fi_calculator.fi_calculator.domain.enums.EngineId;
 import me.fi_calculator.fi_calculator.domain.generic.ApiResponse;
+import me.fi_calculator.fi_calculator.services.calculator.FiCalculatorOrchestrator;
 import me.fi_calculator.fi_calculator.services.calculator.models.FiCalcCommand;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/v1/fi", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CalculatorController {
 
+    private final FiCalculatorOrchestrator fiCalculatorOrchestrator;
+
+    public CalculatorController(FiCalculatorOrchestrator fiCalculatorOrchestrator) {
+        this.fiCalculatorOrchestrator = fiCalculatorOrchestrator;
+    }
+
     @PostMapping(path = "/calculate", consumes = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasAnyRole('USER')")
     public ResponseEntity<ApiResponse<?>> calculate(    @Valid @RequestBody FiRequest req,
                                                         @RequestParam(defaultValue = "MONTE_CARLO") EngineId engine){
         var cmd = new FiCalcCommand(engine, req, ShareContext.get().email());
-
+        //Todo: Determine OutPut
+        fiCalculatorOrchestrator.execute(cmd);
         return null;
     }
 }
