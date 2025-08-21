c33375cc74419972c46b093540983c98ea8e3772
Sepehr <asadiyansepehr@gmail.com>
2025-08-21 14:46:27 +0330

    Add cache to Service

--- src/main/java/me/fi_calculator/fi_calculator/config/app/AppConfig.java
+++ src/main/java/me/fi_calculator/fi_calculator/config/app/AppConfig.java
@@ -1,6 +1,7 @@
 package me.fi_calculator.fi_calculator.config.app;
 
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,11 +9,17 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
+@EnableCaching
 public class AppConfig {
 
     private static final Logger log = LoggerFactory.getLogger(AppConfig.class);
@@ -37,5 +44,17 @@ public class AppConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cm = new CaffeineCacheManager(
+                "usersByEmail"
+        );
+        cm.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(1_000_000)
+                .expireAfterAccess(60, TimeUnit.MINUTES));
+        return cm;
     }
 }
